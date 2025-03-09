746143e8df405dce1ca5b1c56b0a831acb9205c8
Scanf-s <calzone0404@gmail.com>
2025-03-09 23:08:55 +0900

    SpringBoot Initialize

---
+++ src/main/java/com/sullung2yo/seatcatcher/SeatCatcherApiApplication.java
@@ -0,0 +1,13 @@
+package com.sullung2yo.seatcatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatCatcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatCatcherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sullung2yo/seatcatcher/SeatCatcherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sullung2yo.seatcatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatCatcherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
