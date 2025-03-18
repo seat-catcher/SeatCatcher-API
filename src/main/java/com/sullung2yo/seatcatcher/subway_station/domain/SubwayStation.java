eba066eba7a15a4f352e58c6e1a3a51cd47089dc
hus0702 <81548468+hus0702@users.noreply.github.com>
2025-03-18 21:37:45 +0900

    Modify SubwayStation class ver.3
    1. change column name intuitively.
    2. add annotation of column "id".

--- src/main/java/com/sullung2yo/seatcatcher/subway_station/domain/SubwayStation.java
+++ src/main/java/com/sullung2yo/seatcatcher/subway_station/domain/SubwayStation.java
@@ -11,34 +11,35 @@ import com.sullung2yo.seatcatcher.common.domain.BaseEntity;
 @AllArgsConstructor
 @Builder
 // 일단 임의로 테이블 이름을 subway_station 이라고 해두겠습니다.
-@Table(name="subway_station", uniqueConstraints = {@UniqueConstraint(columnNames = {"sbwy_stns_nm", "sbwy_rout_ln"})})
+@Table(name="subway_station", uniqueConstraints = {@UniqueConstraint(columnNames = {"station_name", "line_no"})})
 public class SubwayStation extends BaseEntity{
 
     /*
         BaseEntity 를 상속받아
-        id
-        created_at
-        modified_at
+        id   ::   id 는 지하철 역에 대한 고유 식별자로써, 인덱싱 역할을 수행합니다.
+        created_at    ::    데이터베이스에 저장된 날짜입니다.
+        modified_at    ::    데이터베이스에서 수정된 날짜입니다.
         해당 세 칼럼은 정의하지 않았습니다.
     */
-    @Column(name="sbwy_stns_nm", nullable = false)
-    private String name; // 역의 이름입니다. (subway stations name)
+
+    @Column(name="station_name", nullable = false)
+    private String stationName; // 역의 이름입니다. (subway stations name)
 
     //TODO :: 데이터셋에는 string으로 되어 있습니다만, Jackson이 알아서 변환도 해준다고 해서 int로 뒀습니다. 확인이 필요합니다!
-    @Column(name="sbwy_rout_ln", nullable = false)
-    private int routeLine; // 호선 번호입니다. (subway route line)
+    @Column(name="line_no", nullable = false)
+    private int lineNo; // 호선 번호입니다. (subway route line)
 
     @Column(name="dist_km", nullable = false)
     private float distance; // 전 역에서 해당 역까지의 거리입니다.
 
-    @Column(name="hm", nullable = false)
-    private String hourMinute; // 전 역에서 해당 역까지 오는 데에 걸리는 시간입니다. "분:초" 형식으로 제공됩니다. (hour-minute)
+    @Column(name="time_min_sec", nullable = false)
+    private String timeMinSec; // 전 역에서 해당 역까지 오는 데에 걸리는 시간입니다. "분:초" 형식으로 제공됩니다. (hour-minute)
 
     @Column(name="acml_dist", nullable = false)
     private float accumulateDistance; // 첫 역부터 해당 역까지 도착했을 때 기준 누계 거리입니다.
 
-    @Column(name="acml_hm", nullable = false)
-    private String accumulateHourMinute; // 첫 역부터 해당 역까지 도착했을 때 기준 누계 시간입니다.
+    @Column(name="acml_time", nullable = false)
+    private String accumulateTime; // 첫 역부터 해당 역까지 도착했을 때 기준 누계 시간입니다.
     /*
         TODO :: 해당 칼럼은 데이터 소스에서 제공되지 않습니다! 데이터 삽입을 할 때 주어진 데이터로 계산해야 합니다.
 
@@ -54,12 +55,12 @@ public class SubwayStation extends BaseEntity{
 
     // 전 역에서 해당 역까지 오는 데에 걸리는 시간을 초로 변환해주는 함수입니다!
     public long getHourMinuteInSeconds() {
-        return convertStringToSeconds(hourMinute);
+        return convertStringToSeconds(timeMinSec);
     }
 
     // 누계 시간을 초로 변환해주는 함수입니다!
     public long getAccumulateHourMinuteInSeconds() {
-        return convertStringToSeconds(accumulateHourMinute);
+        return convertStringToSeconds(accumulateTime);
     }
 
     // 소스에서 데이터가 "분:초" 형식으로 오는데, 이걸 넣으면 그게 몇 초인지 리턴해주는 함수입니다!
