a272605b6f0590976a552aa22509101f3146c161
Scanf-s <calzone0404@gmail.com>
2025-03-30 19:25:48 +0900

    Modify swagger configuration (Add authenticate)

--- src/main/java/com/sullung2yo/seatcatcher/config/SpringDocConfig.java
+++ src/main/java/com/sullung2yo/seatcatcher/config/SpringDocConfig.java
@@ -1,9 +1,12 @@
 package com.sullung2yo.seatcatcher.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +15,7 @@ import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
+    private final String securitySchemeName = "Bearer Authentication";
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -31,8 +35,17 @@ public class SpringDocConfig {
                         )
                 )
                 .servers(List.of(
-                        new Server().url("http://api:8080")
+                        new Server().url("https://api.dev.seatcatcher.site")
                                 .description("Docker internal API Server")
-                ));
+                ))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                );
     }
 }
