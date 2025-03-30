3967bd21e97dbed2891fc1bd92513d902c163b59
Scanf-s <calzone0404@gmail.com>
2025-03-30 18:40:50 +0900

    Modify swagger configuration

--- src/main/java/com/sullung2yo/seatcatcher/config/SpringDocConfig.java
+++ src/main/java/com/sullung2yo/seatcatcher/config/SpringDocConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SpringDocConfig {
@@ -26,6 +29,10 @@ public class SpringDocConfig {
                                 .name("Apache 2.0")
                                 .url("https://opensource.org/license/apache-2-0")
                         )
-                );
+                )
+                .servers(List.of(
+                        new Server().url("http://api:8080")
+                                .description("Docker internal API Server")
+                ));
     }
 }
