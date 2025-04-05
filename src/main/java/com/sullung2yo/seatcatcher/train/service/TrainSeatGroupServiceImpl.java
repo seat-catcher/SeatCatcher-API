846075ee845ac1133c5ea2b76580f23ddfc34d68
hus0702 <81548468+hus0702@users.noreply.github.com>
2025-04-05 17:44:29 +0900

    Refactoring TrainSeatGroupServiceImpl
    1. remove unused imports
    2. remove unused references of repositories
    3. change constructor pattern to builder pattern.

--- src/main/java/com/sullung2yo/seatcatcher/train/service/TrainSeatGroupServiceImpl.java
+++ src/main/java/com/sullung2yo/seatcatcher/train/service/TrainSeatGroupServiceImpl.java
@@ -1,36 +1,23 @@
 package com.sullung2yo.seatcatcher.train.service;
 
 import com.sullung2yo.seatcatcher.train.domain.*;
-import com.sullung2yo.seatcatcher.train.repository.TrainSeatGroupRepository;
-import com.sullung2yo.seatcatcher.train.repository.TrainSeatRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Service
 @Slf4j // 이걸 추가하면 로깅을 마음껏 쓸 수 있음.
 public class TrainSeatGroupServiceImpl implements TrainSeatGroupService {
 
-    private final TrainSeatGroupRepository trainSeatGroupRepository;
-    private final TrainSeatRepository trainSeatRepository;
-
-
-    public TrainSeatGroupServiceImpl(TrainSeatGroupRepository trainSeatGroupRepository, TrainSeatRepository trainSeatRepository) {
-        this.trainSeatGroupRepository = trainSeatGroupRepository;
-        this.trainSeatRepository = trainSeatRepository;
-    }
-
     @Override
     public TrainSeatGroup create(TrainCar car, SeatGroupType groupType){
 
-        TrainSeatGroup trainSeatGroup = new TrainSeatGroup(
-                car,
-                new ArrayList<>(),
-                groupType
-        );
+        TrainSeatGroup trainSeatGroup = TrainSeatGroup.builder()
+                .trainCar(car)
+                .trainSeats(new ArrayList<>())
+                .type(groupType)
+                .build();
 
         for(int i = 0; i < groupType.getSeatCount(); i++)
         {
@@ -41,12 +28,12 @@ public class TrainSeatGroupServiceImpl implements TrainSeatGroupService {
             }
             else seatType = SeatType.NORMAL; // 임산부 좌석은 고려하지 않고 일단 Normal 로 모두 설정하겠음. TODO :: 추후에 임산부 좌석이 고려되어야 할 경우 이 부분을 변경할 것.
 
-            TrainSeat trainSeat = new TrainSeat(
-                    trainSeatGroup,
-                    i,
-                    seatType,
-                    0
-            );
+            TrainSeat trainSeat = TrainSeat.builder()
+                    .trainSeatGroup(trainSeatGroup)
+                    .seatLocation(i)
+                    .seatType(seatType)
+                    .jjimCount(0)
+                    .build();
             trainSeatGroup.getTrainSeats().add(trainSeat);
         }
 
@@ -56,6 +43,7 @@ public class TrainSeatGroupServiceImpl implements TrainSeatGroupService {
 
             이 부분은 최종적으로 Train 이 저장될 때 Cascade 에 의해 자동으로 이루어진다. 지금 저장하면 불필요한 중복 저장이 발생!
         */
+
         return trainSeatGroup;
     }
 }
