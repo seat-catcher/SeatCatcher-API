0b40532c15792ccc53028c472cfc14bf0a1c48ac
Scanf-s <calzone0404@gmail.com>
2025-04-06 16:16:41 +0900

    Add one more test case

--- src/test/java/com/sullung2yo/seatcatcher/subway_station/utility/StationDataJsonParserTest.java
+++ src/test/java/com/sullung2yo/seatcatcher/subway_station/utility/StationDataJsonParserTest.java
@@ -4,13 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sullung2yo.seatcatcher.subway_station.dto.SubwayStationData;
 import com.sullung2yo.seatcatcher.subway_station.utility.parser.StationDataJsonParser;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,24 +40,25 @@ class StationDataJsonParserTest {
     }
 
     @Test
-    void testParseSubwayInfoJson() {
-        try {
-            List<SubwayStationData> stations = parser.parseJsonData(filePath);
+    void testInvalidJsonFormat() {
+        String invalidJsonFilePath = "src/test/resources/invalid_subway_info.json";
+        assertThrows(Exception.class, () -> parser.parseJsonData(invalidJsonFilePath));
+    }
 
-            // stations가 null이 아니고, 비어있지 않아야 함
-            assertNotNull(stations, "역 정보 리스트가 null이 아니어야 합니다.");
-            assertFalse(stations.isEmpty(), "역 정보 리스트가 비어 있으면 안 됩니다.");
+    @Test
+    void testParseSubwayInfoJson() throws IOException {
+        List<SubwayStationData> stations = parser.parseJsonData(filePath);
 
-            // 첫 번째 역 검증 (테스트용)
-            log.debug("역 정보 리스트의 첫 번째 역: " + stations.get(0).toString());
-            SubwayStationData first = stations.get(0);
-            assertNotNull(first.getSubwayStationName(), "파싱이 잘못되었습니다. Json 파일을 확인하거나, 파싱 소스코드를 다시 확인해주세요");
-            assertEquals("서울역", first.getSubwayStationName());
-            assertEquals("1", first.getSubwayLine());
-            assertEquals(0.0, first.getDistanceKm());
+        // stations가 null이 아니고, 비어있지 않아야 함
+        assertNotNull(stations, "역 정보 리스트가 null이 아니어야 합니다.");
+        assertFalse(stations.isEmpty(), "역 정보 리스트가 비어 있으면 안 됩니다.");
 
-        } catch (Exception e) {
-            fail("파싱 중 Exception 발생: " + e.getMessage());
-        }
+        // 첫 번째 역 검증 (테스트용)
+        log.debug("역 정보 리스트의 첫 번째 역: " + stations.get(0).toString());
+        SubwayStationData first = stations.get(0);
+        assertNotNull(first.getSubwayStationName(), "파싱이 잘못되었습니다. Json 파일을 확인하거나, 파싱 소스코드를 다시 확인해주세요");
+        assertEquals("서울역", first.getSubwayStationName());
+        assertEquals("1", first.getSubwayLine());
+        assertEquals(0.0, first.getDistanceKm());
     }
 }
