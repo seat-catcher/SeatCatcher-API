d34904f978287cba4ad9e5696eba1eafb85e1040
Scanf-s <calzone-0404@gmail.com>
2025-04-10 01:45:52 +0900

    Remove unused import libs

--- src/test/java/com/sullung2yo/seatcatcher/user/controller/UserControllerTest.java
+++ src/test/java/com/sullung2yo/seatcatcher/user/controller/UserControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
