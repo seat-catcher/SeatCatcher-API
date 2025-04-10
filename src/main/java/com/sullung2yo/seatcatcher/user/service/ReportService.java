ef8e556840efb6addf0e7ab9555cbade66a0fedd
rudeore-098 <rudeore0928@gmail.com>
2025-04-10 18:53:52 +0900

    [FEAT] report 생성 서비스 구현

--- src/main/java/com/sullung2yo/seatcatcher/user/service/ReportService.java
+++ src/main/java/com/sullung2yo/seatcatcher/user/service/ReportService.java
@@ -1,9 +1,16 @@
 package com.sullung2yo.seatcatcher.user.service;
 
 import com.sullung2yo.seatcatcher.user.domain.Report;
+import com.sullung2yo.seatcatcher.user.dto.request.ReportRequest;
 
 import java.util.List;
 
 public interface ReportService {
     public List<Report> getAllReports();
+    public void deleteReport(Long reportId);
+
+//    public ReportResponse updateReport(Long reportId, ReportUpdateRequest request);
+//    public ReportResponse getReportById(Long reportId);
+//    public List<ReportResponse> getMyReports(ReportSearchCondition condition);
+    public void createReport(ReportRequest request);
 }
--- src/main/java/com/sullung2yo/seatcatcher/user/service/ReportServiceImpl.java
+++ src/main/java/com/sullung2yo/seatcatcher/user/service/ReportServiceImpl.java
@@ -1,7 +1,12 @@
 package com.sullung2yo.seatcatcher.user.service;
 
+import com.sullung2yo.seatcatcher.config.exception.ErrorCode;
+import com.sullung2yo.seatcatcher.config.exception.TokenException;
 import com.sullung2yo.seatcatcher.user.domain.Report;
+import com.sullung2yo.seatcatcher.user.domain.User;
+import com.sullung2yo.seatcatcher.user.dto.request.ReportRequest;
 import com.sullung2yo.seatcatcher.user.repository.ReportRepository;
+import com.sullung2yo.seatcatcher.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,9 +18,33 @@ import java.util.List;
 public class ReportServiceImpl implements ReportService{
 
     private final ReportRepository reportRepository;
+    private final UserRepository userRepository;
     @Override
     public List<Report> getAllReports() {
         List<Report> reports = reportRepository.findAll();
         return reports;
+    }
+
+    @Override
+    public void deleteReport(Long reportId) {
+
+    }
+
+    @Override
+    public void createReport(ReportRequest request) {
+
+        User reportUser = userRepository.findById(request.getReportUserId())
+                .orElseThrow(() -> new IllegalArgumentException("id : "+ request.getReportUserId() + "사용자를 찾을 수 없습니다."));
+
+        User reportedUser = userRepository.findById(request.getReportedUserId())
+                .orElseThrow(() -> new IllegalArgumentException("id : "+ request.getReportUserId() + "사용자를 찾을 수 없습니다."));
+
+        Report report = Report.builder()
+                .reportUser(reportUser)
+                .reportedUser(reportedUser)
+                .reason(request.getReason())
+                .build();
+
+        reportRepository.save(report);
     }
 }
