c5a557a6e391d3653fcc0a0dd9727b4ef8185187
Scanf-s <calzone-0404@gmail.com>
2025-04-11 14:57:46 +0900

    Add test case in NameGneratorTest

--- src/test/java/com/sullung2yo/seatcatcher/user/utility/random/NameGeneratorTest.java
+++ src/test/java/com/sullung2yo/seatcatcher/user/utility/random/NameGeneratorTest.java
@@ -1,8 +1,12 @@
 package com.sullung2yo.seatcatcher.user.utility.random;
 
+import com.sullung2yo.seatcatcher.user.domain.Provider;
+import com.sullung2yo.seatcatcher.user.domain.User;
+import com.sullung2yo.seatcatcher.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,24 +15,45 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class NameGeneratorTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private NameGenerator nameGenerator;
+    private static final String originalName = "originalName";
 
     @BeforeEach
     void setUp() {
-        nameGenerator = new NameGenerator();
     }
 
     @Test
     void testGenerateRandomName() {
-        // Given
-        String randomName = "originalName";
-
-        // When
-        randomName = nameGenerator.generateRandomName();
+        // Given, When
+        String randomName = nameGenerator.generateRandomName();
         log.debug(randomName);
 
         // Then
         assertNotNull(randomName);
-        assertNotEquals("originalName", randomName);
+        assertNotEquals(originalName, randomName);
+    }
+
+    @Test
+    void testGenerateRealUser() {
+        // Given
+        User user = User.builder()
+                .provider(Provider.KAKAO)
+                .providerId("testProviderId")
+                .name(nameGenerator.generateRandomName())
+                .build();
+        userRepository.save(user);
+        String originUserName = user.getName();
+
+        // When
+        user.setName(nameGenerator.generateRandomName());
+        userRepository.save(user);
+
+        // Then
+        assertNotEquals(originUserName, user.getName());
+        userRepository.deleteAll();
     }
 }
