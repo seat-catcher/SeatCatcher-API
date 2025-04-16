2e12acf05bf15a75f7749e5646949e487addf58a
Scanf-s <calzone0404@gmail.com>
2025-04-16 16:03:19 +0900

    Attatch Swagger into implemented APIs

--- src/main/java/com/sullung2yo/seatcatcher/user/controller/TagController.java
+++ src/main/java/com/sullung2yo/seatcatcher/user/controller/TagController.java
@@ -1,8 +1,14 @@
 package com.sullung2yo.seatcatcher.user.controller;
 
+import com.sullung2yo.seatcatcher.train.dto.response.CascadeTrainSeatResponse;
 import com.sullung2yo.seatcatcher.user.domain.Tag;
 import com.sullung2yo.seatcatcher.user.dto.response.TagResponse;
 import com.sullung2yo.seatcatcher.user.service.TagService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,6 +26,24 @@ public class TagController {
     private final TagService tagService;
 
     @GetMapping("")
+    @Operation(
+            summary = "선택 가능한 태그 리스트를 조회하는 API",
+            description = "선택 가능한 태그 리스트를 조회하는 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "태그 목록 조회 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = TagResponse.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "태그가 하나도 없음"
+                    )
+            }
+    )
     public ResponseEntity<List<TagResponse>> getTags() {
         // 데이터베이스에 있는 모든 태그 정보 가져오는 API
         List<Tag> tagList = tagService.getTags();
@@ -42,6 +66,23 @@ public class TagController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "id에 해당하는 태그의 정보를 조회하는 API",
+            description = "id에 해당하는 태그의 정보를 조회하는 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "태그 목록 성공",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = TagResponse.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "태그가 하나도 없음"
+                    )
+            }
+    )
     public ResponseEntity<TagResponse> getTagById(@PathVariable Long id) {
         // 태그 ID로 태그 정보 조회하는 API
         Tag tag = tagService.getTagById(id);
