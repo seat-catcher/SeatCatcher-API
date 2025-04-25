305cbc958197011ab64e97900f2a81461efd0e67
Scanf-s <calzone0404@gmail.com>
2025-04-25 20:28:36 +0900

    [REFACTOR] Modified TrainSeatGroup to Train

---
+++ src/main/java/com/sullung2yo/seatcatcher/train/event_handler/SeatEventAssembler.java
@@ -0,0 +1,16 @@
+package com.sullung2yo.seatcatcher.train.event_handler;
+
+import com.sullung2yo.seatcatcher.train.dto.event.SeatEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SeatEventAssembler {
+
+    public List<SeatEvent> assembleSeatEvents() {
+        return null;
+    }
+}
