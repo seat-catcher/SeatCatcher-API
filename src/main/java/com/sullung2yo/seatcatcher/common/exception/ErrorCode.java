149114a7aaaea1790f5b2eb207f35f2f30c400ca
rudeore-098 <rudeore0928@gmail.com>
2025-05-04 11:19:17 +0900

    [MERGE] 병합 충돌 해결

--- src/main/java/com/sullung2yo/seatcatcher/common/exception/ErrorCode.java
+++ src/main/java/com/sullung2yo/seatcatcher/common/exception/ErrorCode.java
@@ -22,10 +22,8 @@ public enum ErrorCode {
     USER_NOT_RESERVED(HttpStatus.BAD_REQUEST, "해당 사용자는 좌석을 점유중이지 않습니다."),
     USER_ALREADY_RESERVED(HttpStatus.BAD_REQUEST, "이미 다른 좌석을 예약한 사용자입니다."),
     TRAIN_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 열차를 찾을 수 없습니다."),
-    INVALID_REQUEST_URI(HttpStatus.BAD_REQUEST,"잘못된 FCM 요청"),
-    FIREBASE_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"Firebase 서버 오류"),
     ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "알람을 찾을 수 없습니다."),
-    ALARM_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 이력에 접근할 수 없습니다."),;
+    ALARM_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 이력에 접근할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
