e155f9209ead86e722da8a407e9924d744bb52f9
hus0702 <81548468+hus0702@users.noreply.github.com>
2025-05-20 22:08:08 +0900

    [ADD] Add how to use

--- src/main/java/com/sullung2yo/seatcatcher/user/service/UserService.java
+++ src/main/java/com/sullung2yo/seatcatcher/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.sullung2yo.seatcatcher.user.service;
 
 
+import com.sullung2yo.seatcatcher.user.domain.CreditPolicy;
 import com.sullung2yo.seatcatcher.user.domain.User;
 import com.sullung2yo.seatcatcher.user.dto.request.UserInformationUpdateRequest;
 
@@ -15,6 +16,7 @@ public interface UserService {
     User increaseCredit(String token, long amount) throws RuntimeException;
     User decreaseCredit(String token, long amount) throws RuntimeException;
     // 내부 로직용 Service. 로직 중간에 크레딧 변경이 일어나야 할 경우 해당 서비스 이용하면 됨.
+    // ex ) increaseCredit(user, CreditPolicy.CREDIT_FOR_SIT_INFO_PROVIDE.getCredit());
     User increaseCredit(User user, long amount) throws RuntimeException;
     User decreaseCredit(User user, long amount) throws RuntimeException;
 }
