0e4f1b4a49b8ea76c3fc532fd3a1f629864e9e9f
Scanf-s <calzone0404@gmail.com>
2025-06-08 15:36:22 +0900

    hotfix(seat)

--- src/main/java/com/sullung2yo/seatcatcher/train/dto/request/UserTrainSeatRequest.java
+++ src/main/java/com/sullung2yo/seatcatcher/train/dto/request/UserTrainSeatRequest.java
@@ -3,7 +3,6 @@ package com.sullung2yo.seatcatcher.train.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
@@ -17,7 +16,6 @@ public class UserTrainSeatRequest {
     @Schema(description = "유저가 앉을 자리의 ID입니다.")
     private Long seatId;
 
-    @NotNull
     @Schema(description = "유저 간에 오고 가는 크레딧 양입니다.")
     private Long creditAmount;
     
