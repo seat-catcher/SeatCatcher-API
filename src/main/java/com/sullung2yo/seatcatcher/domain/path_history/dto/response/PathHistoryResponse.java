f18d38fda96fd04b0f208efedffe5c733fdf7426
kikye04040 <kikye04040@gmail.com>
2025-08-15 03:26:21 +0900

    fix(path_history): 불변 객체 지향을 위한 DTO 수정

--- src/main/java/com/sullung2yo/seatcatcher/domain/path_history/dto/response/PathHistoryResponse.java
+++ src/main/java/com/sullung2yo/seatcatcher/domain/path_history/dto/response/PathHistoryResponse.java
@@ -11,52 +11,50 @@ import java.util.List;
 public class PathHistoryResponse {
 
     @Getter
-    @Setter
     @Builder
     @AllArgsConstructor
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @ToString
     public static class PathHistoryList {
         private List<PathHistoryInfoResponse> pathHistoryInfoList;
         private Long nextCursor; // 다음 커서 값
-        boolean isLast;
+        private boolean isLast;
     }
+
     @Getter
-    @Setter
     @Builder
     @AllArgsConstructor
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @ToString
-    public static class PathHistoryInfoResponse{
+    public static class PathHistoryInfoResponse {
 
         @Schema(description = "PathHistoryId 입니다.")
-        Long id;
+        private Long id;
 
         @Schema(description = "시작역 id 입니다.")
-        Long startStationId;
+        private Long startStationId;
 
         @Schema(description = "시작역 명 입니다.")
-        String startStationName;
+        private String startStationName;
 
         @Schema(description = "시작역 호선 정보입니다.")
-        Line startline;
+        private Line startline;
 
         @Schema(description = "도착역 id 입니다.")
-        Long endStationId;
+        private Long endStationId;
 
         @Schema(description = "도착역 명 입니다.")
-        String endStationName;
+        private String endStationName;
 
         @Schema(description = "도착역 호선 정보입니다.")
-        Line endline;
+        private Line endline;
 
-        //초 제외
+        // 초 제외
         @Schema(description = "예상 도착 시간을 나타냅니다.")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
-        LocalDateTime expectedArrivalTime;
+        private LocalDateTime expectedArrivalTime;
 
         @Schema(description = "경로 생성일을 나타냅니다.")
-        String createdDate;
+        private String createdDate;
     }
-
 }
