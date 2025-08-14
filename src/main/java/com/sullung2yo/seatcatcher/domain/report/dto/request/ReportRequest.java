ce95453901a603699f7e018e709ed5bb1e8557c5
kikye04040 <kikye04040@gmail.com>
2025-08-15 03:28:45 +0900

    fix(report): 불변 객체 지향을 위한 DTO 수정

--- src/main/java/com/sullung2yo/seatcatcher/domain/report/dto/request/ReportRequest.java
+++ src/main/java/com/sullung2yo/seatcatcher/domain/report/dto/request/ReportRequest.java
@@ -1,13 +1,23 @@
 package com.sullung2yo.seatcatcher.domain.report.dto.request;
 
+import com.google.firebase.database.annotations.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
-@Setter
+@Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "신고 요청 DTO")
 public class ReportRequest {
+
+    @Schema(description = "신고자 ID")
     private Long reportUserId;
+
+    @Schema(description = "피신고자 ID")
     private Long reportedUserId;
+
+    @Schema(description = "신고 사유")
     private String reason;
 }
--- src/main/java/com/sullung2yo/seatcatcher/domain/report/dto/response/ReportResponse.java
+++ src/main/java/com/sullung2yo/seatcatcher/domain/report/dto/response/ReportResponse.java
@@ -1,16 +1,27 @@
 package com.sullung2yo.seatcatcher.domain.report.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
-@Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "신고 응답 DTO")
 public class ReportResponse {
+
+    @Schema(description = "신고자 ID")
     private Long reportUserId;
+
+    @Schema(description = "신고자 이름")
     private String reportUserName;
+
+    @Schema(description = "피신고자 ID")
     private Long reportedUserId;
+
+    @Schema(description = "피신고자 이름")
     private String reportedUserName;
+
+    @Schema(description = "신고 사유")
     private String reason;
 }
