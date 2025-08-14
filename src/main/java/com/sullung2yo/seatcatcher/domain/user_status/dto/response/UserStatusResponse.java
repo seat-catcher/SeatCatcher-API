ab87403c2400528331cbdeaa9d0e689a9830d853
kikye04040 <kikye04040@gmail.com>
2025-08-15 03:58:37 +0900

    fix(user_status): 불변 객체 지향을 위한 DTO 수정

--- src/main/java/com/sullung2yo/seatcatcher/domain/user_status/dto/response/UserStatusResponse.java
+++ src/main/java/com/sullung2yo/seatcatcher/domain/user_status/dto/response/UserStatusResponse.java
@@ -3,11 +3,13 @@ package com.sullung2yo.seatcatcher.domain.user_status.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Schema(description = "유저 상태값 Response DTO")
 public class UserStatusResponse {
 
@@ -22,5 +24,4 @@ public class UserStatusResponse {
 
     @Schema(description = "유저가 양보를 요청했던 좌석의 ID (구독 유지를 위함)")
     private Long seatIdRequested;
-
 }
